ed8d33d95f671266308455bf86b7956b9ab36f45
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-03-06 18:23:11 +0100

    Accept mount location as argument in disk component

--- src/disk.rs
+++ src/disk.rs
@@ -2,7 +2,13 @@ mod utils;
 use nix::sys::statvfs::statvfs;
 
 fn main() {
-	let Ok(disk) = statvfs("/") else {
+	let mounted_on;
+	match std::env::args().nth(1) {
+		Some(arg) => mounted_on = arg,
+		None => mounted_on = String::from("/")
+	}
+
+	let Ok(disk) = statvfs(mounted_on.as_str()) else {
 		eprintln!("Could not retrieve disk data");
 		std::process::exit(1);
 	};

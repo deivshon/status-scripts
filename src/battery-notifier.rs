e1dd0fbc377e25239ee1acee598dbd738e8418de
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-07-28 18:40:58 +0200

    feat: :sparkles: add low battery charge notifier script

--- src/battery-notifier.rs
+++ src/battery-notifier.rs
@@ -1,7 +1,17 @@
 pub mod utils;
 
-use utils::{BATTERIES_PATH, CAPACITY_FILE, STATUS_FILE};
+use notify_rust::Notification;
+use std::fs;
+use std::thread::sleep;
+
+use argparse::{ArgumentParser, Store};
 use utils::is_dir_battery;
+use utils::{BATTERIES_PATH, CAPACITY_FILE, STATUS_FILE};
+
+fn failure(msg: &str) -> ! {
+    eprintln!("Failure: {}", msg);
+    std::process::exit(1);
+}
 
 fn main() {
     let Some(battery_path) = utils::first_matching_dir(
@@ -13,9 +23,57 @@ fn main() {
 		std::process::exit(0);
 	};
 
+    let mut threshold: u8 = 15;
+    {
+        let mut ap = ArgumentParser::new();
+
+        ap.refer(&mut threshold).add_option(
+            &["-t", "--threshold"],
+            Store,
+            "Threshold which triggers low battery charge notification",
+        );
+
+        ap.parse_args_or_exit();
+    }
 
     let capacity_path = format!("{}/{}/{}", BATTERIES_PATH, battery_path, CAPACITY_FILE);
     let status_path = format!("{}/{}/{}", BATTERIES_PATH, battery_path, STATUS_FILE);
 
-    println!("{}, {}", capacity_path, status_path)
+    let mut been_notified = false;
+    loop {
+        let current_capacity = match fs::read_to_string(&capacity_path) {
+            Ok(c) => c,
+            Err(_) => failure("Could not get battery capacity"),
+        };
+        let current_capacity = match current_capacity.trim().parse::<u8>() {
+            Ok(c) => c,
+            Err(_) => failure("Could not parse battery capacity"),
+        };
+
+        let current_status = match fs::read_to_string(&status_path) {
+            Ok(s) => s,
+            Err(_) => failure("Could not get battery status"),
+        };
+        let current_status = current_status.trim();
+        let is_charging = current_status == "Charging";
+
+        if been_notified && is_charging {
+            been_notified = false;
+        }
+
+        if current_capacity < threshold && !is_charging && !been_notified {
+            let notification = Notification::new()
+                .summary("Battery low")
+                .body(format!("Battery lower than threshold ({}%)", &threshold).as_str())
+                .timeout(0)
+                .show();
+
+            match notification {
+                Ok(_) => been_notified = true,
+                Err(_) => failure("Could not show notification"),
+            }
+        }
+
+        sleep(std::time::Duration::from_millis(1000));
+    }
 }

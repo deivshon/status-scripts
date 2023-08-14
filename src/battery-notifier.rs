32cee12d6858a2c5ca4d7443793df290cab73a7a
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-08-14 17:03:40 +0200

    feat: :sparkles: add high battery charge threshold

--- src/battery-notifier.rs
+++ src/battery-notifier.rs
@@ -23,14 +23,20 @@ fn main() {
 		std::process::exit(0);
 	};
 
-    let mut threshold: u8 = 15;
+    let mut low_threshold: u8 = 20;
+    let mut high_threshold: u8 = 80;
     {
         let mut ap = ArgumentParser::new();
 
-        ap.refer(&mut threshold).add_option(
-            &["-t", "--threshold"],
+        ap.refer(&mut low_threshold).add_option(
+            &["-l", "--lower-threshold"],
             Store,
             "Threshold which triggers low battery charge notification",
+        );
+        ap.refer(&mut high_threshold).add_option(
+            &["-u", "--upper-threshold"],
+            Store,
+            "Threshold which triggers high battery charge notification if the battery is still charging",
         );
 
         ap.parse_args_or_exit();
@@ -39,7 +45,8 @@ fn main() {
     let capacity_path = format!("{}/{}/{}", BATTERIES_PATH, battery_path, CAPACITY_FILE);
     let status_path = format!("{}/{}/{}", BATTERIES_PATH, battery_path, STATUS_FILE);
 
-    let mut been_notified = false;
+    let mut been_notified_low = false;
+    let mut been_notified_high = false;
     loop {
         let current_capacity = match fs::read_to_string(&capacity_path) {
             Ok(c) => c,
@@ -57,19 +64,39 @@ fn main() {
         let current_status = current_status.trim();
         let is_charging = current_status == "Charging";
 
-        if been_notified && is_charging {
-            been_notified = false;
+        if been_notified_low && is_charging {
+            been_notified_low = false;
+        }
+        if been_notified_high && !is_charging {
+            been_notified_high = false
         }
 
-        if current_capacity < threshold && !is_charging && !been_notified {
+        if current_capacity < low_threshold && !is_charging && !been_notified_low {
             let notification = Notification::new()
-                .summary("Battery low")
-                .body(format!("Battery lower than threshold ({}%)", &threshold).as_str())
+                .summary("Low battery charge")
+                .body(format!("Battery lower than set threshold of {}%", &low_threshold).as_str())
                 .urgency(Urgency::Critical)
                 .show();
 
             match notification {
-                Ok(_) => been_notified = true,
+                Ok(_) => been_notified_low = true,
+                Err(_) => failure("Could not show notification"),
+            }
+        }
+        if current_capacity > high_threshold && is_charging && !been_notified_high {
+            let notification = Notification::new()
+                .summary("High battery charge")
+                .body(
+                    format!(
+                        "You can remove the charging device in order to preserve battery health"
+                    )
+                    .as_str(),
+                )
+                .urgency(Urgency::Critical)
+                .show();
+
+            match notification {
+                Ok(_) => been_notified_high = true,
                 Err(_) => failure("Could not show notification"),
             }
         }

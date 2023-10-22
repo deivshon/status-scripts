683b62018005bfa402e3e726284d25d700708ba1
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-10-22 13:46:46 +0200

    feat: :sparkles: add paru support in updates checker script

--- src/arch-updates.rs
+++ src/arch-updates.rs
@@ -7,17 +7,21 @@ use utils::failure;
 
 const FORMAT_LIST: &[&[&str; 2]] = &[
     &["%p", "pacman updates number"],
-    &["%y", "yay updates number"],
+    &["%y", "aur updates number"],
     &["%t", "total updates number"],
 ];
 
 const CHECKUPDATES_COMMAND: &str = "checkupdates";
 const YAY_COMMAND: &str = "yay";
-const YAY_ARG: &str = "-Qua";
+const PARU_COMMAND: &str = "paru";
+const AUR_HELPER_ARG: &str = "-Qua";
 
 fn main() {
+    let aur_helper_command: &str;
+
     let mut show_format_list = false;
     let mut format: String = String::from("%p/%y");
+    let mut use_paru: bool = false;
     {
         let mut ap = ArgumentParser::new();
 
@@ -30,6 +34,9 @@ fn main() {
             "Show possible format values",
         );
 
+        ap.refer(&mut use_paru)
+            .add_option(&["-p", "--paru"], StoreTrue, "Use paru instead of yay");
+
         ap.parse_args_or_exit();
     }
 
@@ -37,6 +44,11 @@ fn main() {
         utils::print_format_list(FORMAT_LIST);
         std::process::exit(0);
     }
+
+    aur_helper_command = match use_paru {
+        false => YAY_COMMAND,
+        true => PARU_COMMAND,
+    };
 
     let pacman_updates = match Command::new(CHECKUPDATES_COMMAND).output() {
         Ok(o) => o.stdout,
@@ -46,27 +58,30 @@ fn main() {
     };
     let pacman_updates = String::from_utf8_lossy(&pacman_updates).lines().count();
 
-    let yay_updates = match Command::new(YAY_COMMAND).arg(YAY_ARG).output() {
+    let aur_updates = match Command::new(aur_helper_command)
+        .arg(AUR_HELPER_ARG)
+        .output()
+    {
         Ok(o) => o.stdout,
         Err(e) => failure(
             format!(
                 "Could not obtain `{} {}` output: {}",
-                YAY_COMMAND, YAY_ARG, e
+                aur_helper_command, AUR_HELPER_ARG, e
             )
             .as_str(),
         ),
     };
-    let yay_updates = String::from_utf8_lossy(&yay_updates).lines().count();
-    let total_updates = pacman_updates + yay_updates;
+    let aur_updates = String::from_utf8_lossy(&aur_updates).lines().count();
+    let total_updates = pacman_updates + aur_updates;
 
     let pacman_updates = format!("{}", pacman_updates);
-    let yay_updates = format!("{}", yay_updates);
+    let aur_updates = format!("{}", aur_updates);
     let total_updates = format!("{}", total_updates);
 
     let mut data: HashMap<&str, &String> = HashMap::new();
 
     data.insert("%p", &pacman_updates);
-    data.insert("%y", &yay_updates);
+    data.insert("%y", &aur_updates);
     data.insert("%t", &total_updates);
 
     println!("{}", utils::format_output(data, format))
